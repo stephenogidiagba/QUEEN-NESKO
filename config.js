363432bc4c8e35ddfc6e3faec03a018921a12d0e
stephenogidiagba <153740890+stephenogidiagba@users.noreply.github.com>
2025-04-09 21:43:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'XjRizCiK#16S6UTUIbsVMZ4uLMvScEOmG8XkVyungkmZdNO1Ohx0', //add your session id
+SESSION_ID: process.env.SESSION_ID || '', //add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://ayanmodz503:jJJBzHJnZuUkBedM@cluster0.xz3am.mongodb.net/", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/p6Fn0hQf/temp-image.jpg',
 MAX_SIZE: 500
